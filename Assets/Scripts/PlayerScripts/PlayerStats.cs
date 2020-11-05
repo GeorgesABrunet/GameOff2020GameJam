eccecbc0f4718d7895333bb68de28e856f175116
Olivia Maciejewska <oliviamaciejewska@gmail.com>
2020-11-04 21:23:25 -0700

    Updated sliders for belly and thirst

--- Assets/Scripts/PlayerScripts/PlayerStats.cs
+++ Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
@@ -8,14 +9,14 @@ public class PlayerStats : MonoBehaviour
     [Header("Belly")]
     public float MaxBelly = 100f;
     public float Belly = 0f;
-    public float BellyRate = - 0.02f;
-    //public Slider BellySlider;
+    public float BellyRate = 0.02f;
+    public Slider BellySlider;
 
     [Header("Hydration")]
     public float MaxHydration = 100f;
     public float Hydration = 0f;
     public float HydrationRate = 0.02f;
-    //public Slider HydrationSlider;
+    public Slider HydrationSlider;
 
     [Header("Body Heat")]
     public float MaxHeat = 100f;
@@ -32,7 +33,14 @@ public class PlayerStats : MonoBehaviour
     void Start()
     {
         Belly = MaxBelly;
+        BellySlider.maxValue = MaxBelly;
+        BellySlider.value = MaxBelly;
+        
         Hydration = MaxHydration;
+
+        HydrationSlider.maxValue = MaxHydration;
+        HydrationSlider.value = MaxHydration;
+
         Heat = MaxHeat;
         Sanity = MaxSanity;
 
@@ -41,8 +49,11 @@ public class PlayerStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Hydation = Hydration - HydrationRate * Time.deltaTime;
+        Hydration = Hydration - HydrationRate * Time.deltaTime;
+        HydrationSlider.value = Hydration;
+
         Belly = Belly - BellyRate * Time.deltaTime;
+        BellySlider.value = Belly;
 
 
     }
