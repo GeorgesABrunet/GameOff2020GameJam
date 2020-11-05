fa90d10c34efb03d5d8c8c4d39c742f675567c20
GeorgesABrunet <63754259+GeorgesABrunet@users.noreply.github.com>
2020-11-05 14:16:41 -0700

    GameModeAdditions
    Floor Tilemap, Interaction Code, optimization

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -7,8 +7,11 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 _movement;
     private Animator anim;
+
+    private bool BoiIsStanding;
     private void Start()
     {
+        BoiIsStanding = true;
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
@@ -19,21 +22,29 @@ public class PlayerMovement : MonoBehaviour
         float inputX = 0;
 
         //Vertical Movement
-        if ( Input.GetKey(KeyCode.W) )
-        inputY = 3;
-        else if ( Input.GetKey(KeyCode.S) )
-        inputY = -3;
+        if (BoiIsStanding == true)
+        {
+            if ( Input.GetKey(KeyCode.W) )
+            {
+                inputY = 3;
+            }    
+            else if ( Input.GetKey(KeyCode.S) )
+            {
+                inputY = -3;
+            }
+                
 
-        //Horizontal Movement
-        if ( Input.GetKey(KeyCode.D) )
-        {
-            inputX = 3;
-            transform.eulerAngles = new Vector2(0, 180);
-        }
-        else if ( Input.GetKey(KeyCode.A) )
-        {
-            inputX = -3;
-            transform.eulerAngles = new Vector2(0, 0);
+            //Horizontal Movement
+            if ( Input.GetKey(KeyCode.D) )
+            {
+                inputX = 3;
+                transform.eulerAngles = new Vector2(0, 180);
+            }
+            else if ( Input.GetKey(KeyCode.A) )
+            {
+                inputX = -3;
+                transform.eulerAngles = new Vector2(0, 0);
+            }
         }
 
         // Normalize
@@ -45,11 +56,38 @@ public class PlayerMovement : MonoBehaviour
             anim.SetBool("IsWalk", false);
             rb.velocity = _movement * 0;
         }
-
         else
         {
             rb.velocity = _movement * speed;
             anim.SetBool("IsWalk", Math.Abs(_movement.sqrMagnitude) > Mathf.Epsilon);
         }
+
+        //ACTIONS
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (BoiIsStanding == true)
+            {
+                BoiIsStanding = false;
+                anim.SetTrigger("IsSitting");
+                Sit();
+            }
+            else
+            {
+                BoiIsStanding = true;
+                StandUp();
+            }
+        }
+    }
+
+    private void Sit()
+    {
+        anim.SetBool("IsSit", true);
+    }
+
+    private void StandUp()
+    {
+        anim.SetBool("IsSit", false);
+        anim.SetTrigger("IsStanding");
     }
 }
