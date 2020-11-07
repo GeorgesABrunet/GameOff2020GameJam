d377bf35b75883f89b80987bfa02790c7a335fc8
GeorgesABrunet <63754259+GeorgesABrunet@users.noreply.github.com>
2020-11-06 17:10:23 -0700

    Updated Backgrounds

--- Assets/Scripts/GameScripts/ScrollingBackground.cs
+++ Assets/Scripts/GameScripts/ScrollingBackground.cs
@@ -2,20 +2,26 @@
 
 public class ScrollingBackground : MonoBehaviour
 {
- 	private float scrollSpeed = .05f;
-	private Renderer rend;
+ 	private float scrollSpeed = -.1f;
+    Vector2 startPos;
+	//private Renderer rend;
 
 	// Use this for initialization
 	void Start () {
-		rend = GetComponent<Renderer>();
+		startPos = transform.position;
+        //rend = GetComponent<Renderer>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float offset = Time.time * scrollSpeed;
+
+        float newPos = Mathf.Repeat (Time.time * scrollSpeed, 20);
+        transform.position = startPos + Vector2.right * newPos;
+		/*float offset = Time.time * scrollSpeed;
 		// texture offsets shift how the texture is drawn onto the 3D object, skewing its
 		// UV coordinates; this results in a scrolling effect when applied on one axis
         rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
 		//rend.material.SetTextureOffset("_BumpMap", new Vector2(offset, 0));
+        */
 	}
 }
