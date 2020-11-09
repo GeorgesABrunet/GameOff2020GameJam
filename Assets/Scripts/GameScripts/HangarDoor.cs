2f34b6b8b21cdfb92e064ecba2d45c584fe71676
GeorgesABrunet <63754259+GeorgesABrunet@users.noreply.github.com>
2020-11-09 01:45:26 -0700

    Tilemap Update

---
+++ Assets/Scripts/GameScripts/HangarDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangarDoor : MonoBehaviour
+{
+    private Animator doorAnim;
+    void Start()
+    {
+        doorAnim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnim.SetBool("Open", true);
+        }
+    }
+
+     void OnTriggerExit2D()
+    {
+        doorAnim.SetBool("Open", false);
+    }
+}
