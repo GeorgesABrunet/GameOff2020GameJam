15903ebbc87d3c04b6886192ce3b482bda6f912e
GeorgesABrunet <63754259+GeorgesABrunet@users.noreply.github.com>
2020-11-27 15:56:32 -0700

    QuickUIupdate

---
+++ Assets/Scripts/Object Scripts/OxygenStorageDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OxygenStorageDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI ResourceDisplay;
+    private PlayerStats stats;
+    private float oxygenstored;
+
+    void Start()
+    {
+        ResourceDisplay = GetComponent<TMPro.TextMeshProUGUI>();
+        stats = FindObjectOfType<PlayerStats>();
+        oxygenstored = stats.maxOxygen;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oxygenstored = stats.oxygen;
+        ResourceDisplay.text = "Oxygen: " + oxygenstored + " %";
+    }
+}
