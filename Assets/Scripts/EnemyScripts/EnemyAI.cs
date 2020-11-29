c1f81e25855fd7fa9a4af7cd3f79442f6177a10f
Olivia Maciejewska <oliviamaciejewska@gmail.com>
2020-11-29 02:19:29 -0700

    Ai scripts and pathfinding

---
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    private Vector3 spawnPosition;
+    private Vector3 roamingPosition;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        spawnPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //random direction for enemies for testing purposes
+    private Vector3 GetRoamingPosition()
+    {
+        return spawnPosition + GetRandomDirection() * Random.Range(10f, 70f);
+    }
+
+    //random direction for enemies for testing purposes
+    public static Vector3 GetRandomDirection()
+    {
+        return new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, -1f)).normalized;
+    }
+}
---
+++ Assets/Scripts/GameScripts/BattleSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private Transform enemyTransform;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartBattle();
+    }
+
+    // Update is called once per frame
+    private void StartBattle()
+    {
+        Debug.Log("Start Battle");
+
+    }
+}
