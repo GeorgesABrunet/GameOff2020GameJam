b7cea6f2818eeec853316f0d9eeafc07e3bdc3a6
GeorgesABrunet <63754259+GeorgesABrunet@users.noreply.github.com>
2020-12-01 02:20:12 -0700

    Update

--- Assets/Scripts/EnemyScripts/MeleEnemy.cs
+++ Assets/Scripts/EnemyScripts/MeleEnemy.cs
@@ -1,23 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class MeleEnemy : EnemyFollow
 {
-    public Collider2D SwordBox;
+    //public Collider2D axeBox;
+    public PlayerCombat BlockPos;
+    public bool enemySwinging;
+    public int enemySwingdamage = 12;
+    private int ClobberingTime;
 
-    public int damage = 5;
+    public void EnemyEnableAttack()
+    {
+        enemySwinging = true;
+    }
 
+    public void EnemyDisableAttack()
+    {
+        enemySwinging = false;
+    }
     protected override void Attack()
     {
-        base.Attack();
-    }
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Destructable")
+        //base.Attack();
+        base.enemyAnim.SetBool("IsWalk", true);
+
+        ClobberingTime = UnityEngine.Random.Range(1,5);
+        base.enemyAnim.SetTrigger("IsSwing"+ClobberingTime);
+        if (ClobberingTime == 5)
         {
-            collision.SendMessage("EnemyTakeDamage", damage, SendMessageOptions.DontRequireReceiver);
-            Debug.Log("Get rektd bitch");
+            base.enemyAnim.SetTrigger("IsSwing1");
+        }/*
+        else if (ClobberingTime == 2)
+        {
+            base.enemyAnim.SetTrigger("IsSwing2");
+        }
+        else if (ClobberingTime == 3)
+        {
+            base.enemyAnim.SetTrigger("IsSwing3");
+        }
+        else
+        {
+            base.enemyAnim.SetTrigger("IsSwing4");
+        }*/
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (enemySwinging)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                if (BlockPos.IsBlocking == false)
+                {
+                    Debug.Log("Eat dick bitch");
+                    other.GetComponent<PlayerHealth>().TakeDamage(enemySwingdamage);
+                }
+            } 
+                
+            if (other.gameObject.tag == "Destructable")
+            {
+                Debug.Log("Get rektd, you hunko metal");
+                other.SendMessage("TakeDamage", enemySwingdamage, SendMessageOptions.DontRequireReceiver);
+            }
         }
     }
 }
--- Assets/Scripts/PlayerScripts/Projectile.cs
+++ Assets/Scripts/PlayerScripts/Projectile.cs
@@ -14,7 +14,7 @@ public class Projectile : MonoBehaviour
 
     public GameObject destroyEffect;
 
-    public int damage = 20;
+    public int damage = 15;
 
     private void Start()
     {
@@ -22,32 +22,33 @@ public class Projectile : MonoBehaviour
     }
     private void Update()
     {
-        /*
+        
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
         if (hitInfo.collider != null)
         {
             if (hitInfo.collider.CompareTag("Enemy"))
             {
                 Debug.Log("enemy rektd");
+                hitInfo.collider.SendMessage("EnemyTakeDamage", damage, SendMessageOptions.DontRequireReceiver);
             }
             DestroyProjectile();   
         }
-        */
+        
         transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    /*void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform != this.transform)
         {
             other.SendMessage("EnemyTakeDamage", damage, SendMessageOptions.DontRequireReceiver);
             Destroy(gameObject);
         }
-    }
+    }*/
     
     void DestroyProjectile()
     {
-        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        //Instantiate(destroyEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
